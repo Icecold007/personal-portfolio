d05db6a776057e7d391351f468568aeab9189055
Anurag Saud <anuragsaud05@gmail.com>
2024-12-13 02:13:56 +0530

    lol 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,23 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Anurag Saud</title>
+	<meta name="description" content="Bringing ideas to life." />
+	<meta content="Anurag Saud" property="og:title" />
+	<meta
+		content="https://ogcdn.net/91e635e5-1054-4d92-b574-2b9c4178edb2/v1/https%3A%2F%2Fanuragsaud.com%2F/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F2028fd6d-c8e2-42d2-8ed5-9d5342338838.jpg%3Ftoken%3DcEilbjWPJeZHVFSlR78WYfaQ0pWaJdDKAyVXL3HBgow%26height%3D800%26width%3D1200%26expires%3D33270035783/My%20portfolio%20website/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F049241ed-4bd6-4609-b157-4366b66d8d6d.jpg%3Ftoken%3DHmssgU8g702EZSFov5Ly_vrYifRcn91yUL9yQo-BBZw%26height%3D800%26width%3D1200%26expires%3D33270035812/Anurag%20Saud/og.png"
+		property="og:image"
+	/>
+	<meta content="https://anuragsaud.com/" property="og:url" />
+	<meta content="Bringing ideas to life." property="og:description" />
+	<meta content="Anurag Saud" property="og:site_name" />
+	<meta content="Anurag Saud" name="twitter:creator" />
+	<meta content="Anurag Saud" name="twitter:title" />
+	<meta content="Bringing ideas to life." name="twitter:description" />
+	<meta
+		content="https://ogcdn.net/91e635e5-1054-4d92-b574-2b9c4178edb2/v1/https%3A%2F%2Fanuragsaud.com%2F/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F2028fd6d-c8e2-42d2-8ed5-9d5342338838.jpg%3Ftoken%3DcEilbjWPJeZHVFSlR78WYfaQ0pWaJdDKAyVXL3HBgow%26height%3D800%26width%3D1200%26expires%3D33270035783/My%20portfolio%20website/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F049241ed-4bd6-4609-b157-4366b66d8d6d.jpg%3Ftoken%3DHmssgU8g702EZSFov5Ly_vrYifRcn91yUL9yQo-BBZw%26height%3D800%26width%3D1200%26expires%3D33270035812/Anurag%20Saud/og.png"
+		name="twitter:image"
+	/>
+</svelte:head>
 <slot />
