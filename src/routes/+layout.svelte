bb994b67ccf08efa0150ccf54190c63e1771f67f
Anurag Saud <anuragsaud05@gmail.com>
2024-12-13 02:26:43 +0530

    lol 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,31 @@
 </script>
 
 <svelte:head>
+	<!-- HTML Meta Tags -->
 	<title>Anurag Saud</title>
 	<meta name="description" content="Bringing ideas to life." />
-	<meta content="Anurag Saud" property="og:title" />
-	<meta content="https://anuragsaud.com/og-image.png" property="og:image" />
-	<meta content="https://anuragsaud.com/" property="og:url" />
-	<meta content="Bringing ideas to life." property="og:description" />
-	<meta content="Anurag Saud" property="og:site_name" />
-	<meta content="Anurag Saud" name="twitter:creator" />
-	<meta content="Anurag Saud" name="twitter:title" />
-	<meta content="Bringing ideas to life." name="twitter:description" />
-	<meta content="https://anuragsaud.com/og-image.png" name="twitter:image" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://anuragsaud.com" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Anurag Saud" />
+	<meta property="og:description" content="Bringing ideas to life." />
+	<meta
+		property="og:image"
+		content="https://opengraph.b-cdn.net/production/images/2140e107-e3b9-4f6d-96bb-d932d810478a.png?token=xv9nMjGBWar7ar0j6EiGuZkVMxnGLsPDGU1vjHcXyHI&height=630&width=1200&expires=33270036949"
+	/>
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="anuragsaud.com" />
+	<meta property="twitter:url" content="https://anuragsaud.com" />
+	<meta name="twitter:title" content="Anurag Saud" />
+	<meta name="twitter:description" content="Bringing ideas to life." />
+	<meta
+		name="twitter:image"
+		content="https://opengraph.b-cdn.net/production/images/2140e107-e3b9-4f6d-96bb-d932d810478a.png?token=xv9nMjGBWar7ar0j6EiGuZkVMxnGLsPDGU1vjHcXyHI&height=630&width=1200&expires=33270036949"
+	/>
+
+	<!-- Meta Tags Generated via https://www.opengraph.xyz -->
 </svelte:head>
 <slot />
